85410804c49d577b6680521b95229475818623a2
Daichi Fukase <it.da.fukase@iftc.co.jp>
2026-08-20 12:57:30 +0900

    feat: ゲーム開始クリックで hero レイヤーをフェードアウトする

--- source/dom.ts
+++ source/dom.ts
@@ -6,3 +6,4 @@ export const minimap_canvas = document.getElementById('m') as HTMLCanvasElement
 export const terminal_el = document.getElementById('a') as HTMLElement
 export const nicotine_bar = document.getElementById('n') as HTMLElement
 export const nicotine_fill = document.getElementById('nf') as HTMLElement
+export const hero_el = document.getElementById('h') as HTMLElement
